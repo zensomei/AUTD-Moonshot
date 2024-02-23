514e97e20932f8eca254c47c7fffa3a88b8515c7
z_somei <zsomei621@gmail.com>
2024-02-24 00:44:59 +0900

    develop_240223

--- Assets/My Assets/MultiDevice/AUTDMultiDevice.cs
+++ Assets/My Assets/MultiDevice/AUTDMultiDevice.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using AUTD3Sharp;
 using AUTD3Sharp.Link;
 using AUTD3Sharp.NativeMethods;
+using Intel.RealSense;
 using UnityEngine;
 
 #if UNITY_2020_2_OR_NEWER
@@ -17,6 +18,16 @@ public class AUTDMultiDevice : MonoBehaviour
     private Vector3 _oldPosition;
 
     private static bool _isPlaying = true;
+
+
+    ////Self
+    private PointsCalculator _pointscalculator;
+
+    // Camera_Realsenseオブジェクトを格納するための変数
+    //private Transform? cameraRealsenseTransform;
+    Vector3 realsensePosition;
+    Quaternion realsenseRotation;
+    Vector3 relativePosition;
 
 
     private async void Awake()
@@ -70,8 +81,21 @@ public class AUTDMultiDevice : MonoBehaviour
             _oldPosition = Target.transform.position;
         }
     }
+    void Start()
+    {
+        _pointscalculator = GameObject.Find("PointCloud").GetComponent<PointsCalculator>();
 
-    private async void Update()
+        // シーン内からCamera_Realsenseオブジェクトを検索し、そのTransformを取得
+        GameObject cameraRealsense = GameObject.Find("Camera_Realsense");
+        if (cameraRealsense != null)
+        {
+            realsensePosition = cameraRealsense.transform.position;
+            realsenseRotation = cameraRealsense.transform.rotation;
+            Debug.Log($"Camera_Realsense Position: {realsensePosition}, Rotation: {realsenseRotation.eulerAngles}");
+        }
+    }
+
+    private async void LateUpdate()
     {
 #if UNITY_EDITOR
         if (!_isPlaying)
@@ -82,9 +106,28 @@ public class AUTDMultiDevice : MonoBehaviour
 #endif
         if (_autd == null) return;
 
+        //relativePosition = _pointscalculator.Center;
+        if (Target != null) Target.transform.position = ConvertPosition(relativePosition);
+
+        //if (Target != null) Target.transform.position = realsensePosition + (realsenseRotation * _pointscalculator.Center);
+        //Debug.Log($"relativePosition: {_pointscalculator.Center}");
+
         if (Target == null || Target.transform.position == _oldPosition) return;
         await _autd.SendAsync(new AUTD3Sharp.Gain.Focus(Target.transform.position));
         _oldPosition = Target.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        relativePosition = _pointscalculator.Center;
+        //Debug.Log($"relativePosition: {_pointscalculator.Center}");
+    }
+
+    Vector3 ConvertPosition(Vector3 relativePosition)
+    {
+        Vector3 RealPosition = realsensePosition + (realsenseRotation * relativePosition);
+        //Debug.Log($"relativePosition: {relativePosition}, RealPosition: {RealPosition}");
+        return RealPosition;
     }
 
     private void OnApplicationQuit()
---
+++ Assets/My Assets/MultiDevice/PointsCalculator.cs
@@ -0,0 +1,222 @@
+using System;
+using UnityEngine;
+using Intel.RealSense;
+using UnityEngine.Rendering;
+using UnityEngine.Assertions;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class PointsCalculator : MonoBehaviour
+{
+    public RsFrameProvider Source;
+    private Mesh mesh;
+    private Texture2D uvmap;
+
+    [NonSerialized]
+    private Vector3[] vertices;
+
+    FrameQueue q;
+
+    void Start()
+    {
+        Source.OnStart += OnStartStreaming;
+        Source.OnStop += Dispose;
+    }
+
+    private void OnStartStreaming(PipelineProfile obj)
+    {
+        q = new FrameQueue(1);
+
+        using (var depth = obj.Streams.FirstOrDefault(s => s.Stream == Stream.Depth && s.Format == Format.Z16).As<VideoStreamProfile>())
+            ResetMesh(depth.Width, depth.Height);
+
+        Source.OnNewSample += OnNewSample;
+    }
+
+    private void ResetMesh(int width, int height)
+    {
+        Assert.IsTrue(SystemInfo.SupportsTextureFormat(TextureFormat.RGFloat));
+        uvmap = new Texture2D(width, height, TextureFormat.RGFloat, false, true)
+        {
+            wrapMode = TextureWrapMode.Clamp,
+            filterMode = FilterMode.Point,
+        };
+        GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_UVMap", uvmap);
+
+        if (mesh != null)
+            mesh.Clear();
+        else
+            mesh = new Mesh()
+            {
+                indexFormat = IndexFormat.UInt32,
+            };
+
+        vertices = new Vector3[width * height];
+
+        var indices = new int[vertices.Length];
+        for (int i = 0; i < vertices.Length; i++)
+            indices[i] = i;
+
+        mesh.MarkDynamic();
+        mesh.vertices = vertices;
+
+        var uvs = new Vector2[width * height];
+        Array.Clear(uvs, 0, uvs.Length);
+        for (int j = 0; j < height; j++)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                uvs[i + j * width].x = i / (float)width;
+                uvs[i + j * width].y = j / (float)height;
+            }
+        }
+
+        mesh.uv = uvs;
+
+        mesh.SetIndices(indices, MeshTopology.Points, 0, false);
+        mesh.bounds = new Bounds(Vector3.zero, Vector3.one * 10f);
+
+        GetComponent<MeshFilter>().sharedMesh = mesh;
+    }
+
+    //void OnDestroy()
+    //{
+    //    if (q != null)
+    //    {
+    //        q.Dispose();
+    //        q = null;
+    //    }
+
+    //    if (mesh != null)
+    //        Destroy(null);
+    //}
+
+    void OnDestroy()
+    {
+        if (q != null)
+        {
+            q.Dispose();
+            q = null;
+        }
+
+        if (mesh != null)
+        {
+            Destroy(mesh);
+        }
+    }
+
+    private void Dispose()
+    {
+        Source.OnNewSample -= OnNewSample;
+
+        if (q != null)
+        {
+            q.Dispose();
+            q = null;
+        }
+    }
+
+    private void OnNewSample(Frame frame)
+    {
+        if (q == null)
+            return;
+        try
+        {
+            if (frame.IsComposite)
+            {
+                using (var fs = frame.As<FrameSet>())
+                using (var points = fs.FirstOrDefault<Points>(Stream.Depth, Format.Xyz32f))
+                {
+                    if (points != null)
+                    {
+                        q.Enqueue(points);
+                    }
+                }
+                return;
+            }
+
+            if (frame.Is(Extension.Points))
+            {
+                q.Enqueue(frame);
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+        }
+    }
+
+    private Vector3 center;
+    public Vector3 Center
+    {
+        get { return this.center; }
+        private set { this.center = value; }
+    }
+
+    protected void Update()
+    {
+        if (q != null)
+        {
+            Points points;
+            if (q.PollForFrame<Points>(out points))
+                using (points)
+                {
+                    if (points.Count != mesh.vertexCount)
+                    {
+                        using (var p = points.GetProfile<VideoStreamProfile>())
+                            ResetMesh(p.Width, p.Height);
+                    }
+
+                    if (points.TextureData != IntPtr.Zero)
+                    {
+                        uvmap.LoadRawTextureData(points.TextureData, points.Count * sizeof(float) * 2);
+                        uvmap.Apply();
+                    }
+
+                    if (points.VertexData != IntPtr.Zero)
+                    {
+                        points.CopyVertices(vertices);
+
+                        mesh.vertices = vertices;
+                        mesh.UploadMeshData(false);
+
+                        // 閾値内の点群の中心座標を計算
+                        Vector3 sum = Vector3.zero;
+                        int count = 0;
+                        foreach (var vertex in vertices)
+                        {
+                            // 条件に合致するかチェック
+                            if (vertex.z > 0.1f && vertex.z <= 0.35f && vertex.x > -0.4f && vertex.x <= 0.2f)
+                            {
+                                // Y座標の上下を逆にする
+                                Vector3 invertedYVertex = new Vector3(vertex.x, -vertex.y, vertex.z);
+
+                                sum += invertedYVertex;
+                                count++;
+                            }
+                        }
+
+                        if (count > 10000)
+                        {
+                            // 条件に合致する点群の数を出力
+                            //Debug.Log($"Filtered points count: {count}");
+
+                            // 中心座標を計算
+                            center = sum / count;
+
+                            // 中心座標を出力
+                            Debug.Log($"{count}points, Center of points (Y inverted): {center}");
+                        }
+                        else
+                        {
+                            Debug.Log("Points < 10000.");
+                        }
+                    }
+
+                }
+        }
+    }
+}
